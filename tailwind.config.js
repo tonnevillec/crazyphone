145841c31213ae3e38c371bc495a98ee3e0281e8
Cyril <tonnevillec@gmail.com>
2023-07-22 19:15:37 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const plugin = require("tailwindcss/plugin")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',
+    'assets/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["emerald", "night"],
+    darkTheme: "night",
+    utils: true,
+  },
+}
+
